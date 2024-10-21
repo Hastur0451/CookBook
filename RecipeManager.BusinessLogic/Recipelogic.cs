ecf7d8cd851503026ec06849a850eda6f78aa4df
Hastur0451 <zy20020220@gmail.com>
2024-10-21 21:49:05 +1100

    block caculate

--- RecipeManager.BusinessLogic/Recipelogic.cs
+++ RecipeManager.BusinessLogic/Recipelogic.cs
@@ -7,14 +7,13 @@ namespace RecipeManager.BusinessLogic
     public class RecipeLogic
     {
         private readonly TheMealDBClient _mealDbClient;
-        private readonly FatSecretApiClient _fatSecretClient;
+        // private readonly FatSecretApiClient _fatSecretClient;
 
         public RecipeLogic(string fatSecretConsumerKey, string fatSecretConsumerSecret)
         {
             _mealDbClient = new TheMealDBClient();
-            _fatSecretClient = new FatSecretApiClient(fatSecretConsumerKey, fatSecretConsumerSecret);
+            // _fatSecretClient = new FatSecretApiClient(fatSecretConsumerKey, fatSecretConsumerSecret);
         }
-
 
         public async Task<List<RecipeSearchResult>> SearchRecipes(string searchTerm)
         {
@@ -41,30 +40,40 @@ namespace RecipeManager.BusinessLogic
             return formattedIngredients;
         }
 
-        public async Task<(List<IngredientNutrition> IngredientsNutrition, NutritionInfo TotalNutrition)> CalculateRecipeNutrition(Recipe recipe)
+        /*public async Task<(List<IngredientNutrition> IngredientsNutrition, NutritionInfo TotalNutrition)> CalculateRecipeNutrition(Recipe recipe)
         {
             var ingredientsNutrition = new List<IngredientNutrition>();
             var totalNutrition = new NutritionInfo();
+            var random = new Random();
 
             foreach (var (measure, ingredient) in GetFormattedIngredients(recipe))
             {
-                var query = $"{measure} {ingredient}";
-                var nutrition = await _fatSecretClient.GetFoodNutritionAsync(query);
+                // 使用mock数据替代API调用
+                var mockNutrition = new NutritionInfo
+                {
+                    Calories = random.Next(50, 300),
+                    Protein = random.Next(1, 20),
+                    Carbohydrates = random.Next(5, 50),
+                    Fat = random.Next(1, 15)
+                };
 
                 ingredientsNutrition.Add(new IngredientNutrition
                 {
                     Ingredient = $"{measure} {ingredient}",
-                    Nutrition = nutrition
+                    Nutrition = mockNutrition
                 });
 
-                totalNutrition.Calories += nutrition.Calories;
-                totalNutrition.Protein += nutrition.Protein;
-                totalNutrition.Carbohydrates += nutrition.Carbohydrates;
-                totalNutrition.Fat += nutrition.Fat;
+                totalNutrition.Calories += mockNutrition.Calories;
+                totalNutrition.Protein += mockNutrition.Protein;
+                totalNutrition.Carbohydrates += mockNutrition.Carbohydrates;
+                totalNutrition.Fat += mockNutrition.Fat;
             }
 
+            // 模拟API调用的延迟
+            await Task.Delay(1000);
+
             return (ingredientsNutrition, totalNutrition);
-        }
+        }*/
     }
 
     public class IngredientNutrition
@@ -72,4 +81,12 @@ namespace RecipeManager.BusinessLogic
         public string Ingredient { get; set; }
         public NutritionInfo Nutrition { get; set; }
     }
+
+    /*public class NutritionInfo
+    {
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fat { get; set; }
+    }*/
 }
--- obj/Debug/net8.0/CookBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/CookBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bdb3295f102acfd4786aa2f72801bfba55599f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24bf477ad73e963572802ef4e94f95b964eab863")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
