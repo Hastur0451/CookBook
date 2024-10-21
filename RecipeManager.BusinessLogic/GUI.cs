24bf477ad73e963572802ef4e94f95b964eab863
Hastur0451 <zy20020220@gmail.com>
2024-10-21 20:13:24 +1100

    update search

--- RecipeManager.BusinessLogic/GUI.cs
+++ RecipeManager.BusinessLogic/GUI.cs
@@ -1,21 +1,25 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using System.Threading.Tasks;
+using System.Runtime.Versioning;
 using RecipeManager.BusinessLogic;
-using System.DirectoryServices;
 
 namespace RecipeManager.GUI
 {
+    [SupportedOSPlatform("windows")]
     public partial class RecipeSearchForm : Form
     {
-        private readonly TheMealDBClient _client;
+        private readonly RecipeLogic _recipeLogic;
         private List<RecipeSearchResult> searchResults;
 
         public RecipeSearchForm()
         {
             InitializeComponent();
-            _client = new TheMealDBClient();
+            string fatSecretConsumerKey = "614e76da537c4a61a07a73763b373951";
+            string fatSecretConsumerSecret = "f72938e96e4e4ee7bf42873070c91110";
+            _recipeLogic = new RecipeLogic(fatSecretConsumerKey, fatSecretConsumerSecret);
         }
 
         private void InitializeComponent()
@@ -23,35 +27,55 @@ namespace RecipeManager.GUI
             this.txtSearchTerm = new TextBox();
             this.btnSearch = new Button();
             this.lstResults = new ListBox();
+            this.txtIngredients = new RichTextBox();
+            this.txtNutrition = new RichTextBox();
             this.SuspendLayout();
 
             // txtSearchTerm
-            this.txtSearchTerm.Location = new System.Drawing.Point(12, 12);
+            this.txtSearchTerm.Location = new Point(12, 12);
             this.txtSearchTerm.Name = "txtSearchTerm";
-            this.txtSearchTerm.Size = new System.Drawing.Size(200, 20);
+            this.txtSearchTerm.Size = new Size(250, 20);
             this.txtSearchTerm.TabIndex = 0;
 
             // btnSearch
-            this.btnSearch.Location = new System.Drawing.Point(218, 10);
+            this.btnSearch.Location = new Point(268, 10);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.Size = new Size(75, 23);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "搜索";
             this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.btnSearch.Click += new EventHandler(this.btnSearch_Click);
 
             // lstResults
             this.lstResults.FormattingEnabled = true;
-            this.lstResults.Location = new System.Drawing.Point(12, 38);
+            this.lstResults.Location = new Point(12, 38);
             this.lstResults.Name = "lstResults";
-            this.lstResults.Size = new System.Drawing.Size(281, 212);
+            this.lstResults.Size = new Size(331, 324);
             this.lstResults.TabIndex = 2;
-            this.lstResults.SelectedIndexChanged += new System.EventHandler(this.lstResults_SelectedIndexChanged);
+            this.lstResults.SelectedIndexChanged += new EventHandler(this.lstResults_SelectedIndexChanged);
+
+            // txtIngredients
+            this.txtIngredients.Location = new Point(349, 38);
+            this.txtIngredients.Name = "txtIngredients";
+            this.txtIngredients.ReadOnly = true;
+            this.txtIngredients.Size = new Size(400, 324);
+            this.txtIngredients.TabIndex = 3;
+            this.txtIngredients.Text = "";
+
+            // txtNutrition
+            this.txtNutrition.Location = new Point(755, 38);
+            this.txtNutrition.Name = "txtNutrition";
+            this.txtNutrition.ReadOnly = true;
+            this.txtNutrition.Size = new Size(250, 324);
+            this.txtNutrition.TabIndex = 4;
+            this.txtNutrition.Text = "";
 
             // RecipeSearchForm
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new SizeF(6F, 13F);
             this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 262);
+            this.ClientSize = new Size(1020, 374);
+            this.Controls.Add(this.txtNutrition);
+            this.Controls.Add(this.txtIngredients);
             this.Controls.Add(this.lstResults);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearchTerm);
@@ -64,11 +88,16 @@ namespace RecipeManager.GUI
         private TextBox txtSearchTerm;
         private Button btnSearch;
         private ListBox lstResults;
+        private RichTextBox txtIngredients;
+        private RichTextBox txtNutrition;
 
         private async void btnSearch_Click(object sender, EventArgs e)
         {
             lstResults.Items.Clear();
+            txtIngredients.Clear();
+            txtNutrition.Clear();
             var searchTerm = txtSearchTerm.Text;
+
             if (string.IsNullOrWhiteSpace(searchTerm))
             {
                 MessageBox.Show("请输入搜索词");
@@ -77,10 +106,10 @@ namespace RecipeManager.GUI
 
             try
             {
-                searchResults = await _client.SearchMeals(searchTerm);
+                searchResults = await _recipeLogic.SearchRecipes(searchTerm);
                 foreach (var result in searchResults)
                 {
-                    lstResults.Items.Add(result.Name);  // 直接添加食谱名称
+                    lstResults.Items.Add(result.Name);
                 }
 
                 if (searchResults.Count == 0)
@@ -101,9 +130,14 @@ namespace RecipeManager.GUI
                 var selectedResult = searchResults[lstResults.SelectedIndex];
                 try
                 {
-                    var recipe = await _client.GetRecipeById(selectedResult.Id);
-                    var detailsForm = new RecipeDetailsForm(recipe);
-                    detailsForm.Show();
+                    var recipe = await _recipeLogic.GetRecipeDetails(selectedResult.Id);
+                    DisplayRecipeIngredients(recipe);
+
+                    txtNutrition.Clear();
+                    txtNutrition.AppendText("正在分析营养信息...\n请稍候");
+
+                    var (ingredientsNutrition, totalNutrition) = await _recipeLogic.CalculateRecipeNutrition(recipe);
+                    DisplayNutritionInfo(ingredientsNutrition, totalNutrition);
                 }
                 catch (Exception ex)
                 {
@@ -111,54 +145,42 @@ namespace RecipeManager.GUI
                 }
             }
         }
-    }
 
-    public class RecipeDetailsForm : Form
-    {
-        public RecipeDetailsForm(Recipe recipe)
+        private void DisplayRecipeIngredients(Recipe recipe)
         {
-            InitializeComponent(recipe);
-        }
+            txtIngredients.Clear();
+            txtIngredients.SelectionFont = new Font(txtIngredients.Font, FontStyle.Bold);
+            txtIngredients.AppendText($"{recipe.Name}\n\n");
+            txtIngredients.SelectionFont = txtIngredients.Font;
 
-        private void InitializeComponent(Recipe recipe)
-        {
-            this.txtDetails = new TextBox();
-            this.SuspendLayout();
+            txtIngredients.AppendText("食材:\n\n");
 
-            // txtDetails
-            this.txtDetails.Location = new System.Drawing.Point(12, 12);
-            this.txtDetails.Multiline = true;
-            this.txtDetails.Name = "txtDetails";
-            this.txtDetails.ReadOnly = true;
-            this.txtDetails.ScrollBars = ScrollBars.Vertical;
-            this.txtDetails.Size = new System.Drawing.Size(360, 337);
-            this.txtDetails.TabIndex = 0;
-
-            // RecipeDetailsForm
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 361);
-            this.Controls.Add(this.txtDetails);
-            this.Name = "RecipeDetailsForm";
-            this.Text = "食谱详情";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-            // Populate details
-            var details = $"名称: {recipe.Name}\r\n";
-            details += $"类别: {recipe.Category}\r\n";
-            details += $"区域: {recipe.Area}\r\n\r\n";
-            details += "食材:\r\n";
-            for (int i = 0; i < recipe.Ingredients.Count; i++)
+            foreach (var (measure, ingredient) in _recipeLogic.GetFormattedIngredients(recipe))
             {
-                details += $"- {recipe.Measures[i]} {recipe.Ingredients[i]}\r\n";
+                txtIngredients.AppendText($"• {measure} {ingredient}\n");
             }
-            details += "\r\n烹饪步骤:\r\n";
-            details += recipe.Instructions;
-
-            this.txtDetails.Text = details;
         }
 
-        private TextBox txtDetails;
+        private void DisplayNutritionInfo(List<IngredientNutrition> ingredientsNutrition, NutritionInfo totalNutrition)
+        {
+            txtNutrition.Clear();
+            txtNutrition.SelectionFont = new Font(txtNutrition.Font, FontStyle.Bold);
+            txtNutrition.AppendText("总营养信息:\n\n");
+            txtNutrition.SelectionFont = txtNutrition.Font;
+
+            txtNutrition.AppendText($"总卡路里: {totalNutrition.Calories:F2} kcal\n");
+            txtNutrition.AppendText($"总蛋白质: {totalNutrition.Protein:F2} g\n");
+            txtNutrition.AppendText($"总碳水化合物: {totalNutrition.Carbohydrates:F2} g\n");
+            txtNutrition.AppendText($"总脂肪: {totalNutrition.Fat:F2} g\n\n");
+
+            txtNutrition.SelectionFont = new Font(txtNutrition.Font, FontStyle.Bold);
+            txtNutrition.AppendText("各食材卡路里:\n\n");
+            txtNutrition.SelectionFont = txtNutrition.Font;
+
+            foreach (var item in ingredientsNutrition)
+            {
+                txtNutrition.AppendText($"{item.Ingredient}: {item.Nutrition.Calories:F2} kcal\n");
+            }
+        }
     }
 }
---
+++ RecipeManager.BusinessLogic/Recipelogic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RecipeManager.BusinessLogic
+{
+    public class RecipeLogic
+    {
+        private readonly TheMealDBClient _mealDbClient;
+        private readonly FatSecretApiClient _fatSecretClient;
+
+        public RecipeLogic(string fatSecretConsumerKey, string fatSecretConsumerSecret)
+        {
+            _mealDbClient = new TheMealDBClient();
+            _fatSecretClient = new FatSecretApiClient(fatSecretConsumerKey, fatSecretConsumerSecret);
+        }
+
+
+        public async Task<List<RecipeSearchResult>> SearchRecipes(string searchTerm)
+        {
+            return await _mealDbClient.SearchMeals(searchTerm);
+        }
+
+        public async Task<Recipe> GetRecipeDetails(string recipeId)
+        {
+            return await _mealDbClient.GetRecipeById(recipeId);
+        }
+
+        public List<(string Measure, string Ingredient)> GetFormattedIngredients(Recipe recipe)
+        {
+            var formattedIngredients = new List<(string Measure, string Ingredient)>();
+
+            for (int i = 0; i < recipe.Ingredients.Count; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(recipe.Ingredients[i]))
+                {
+                    formattedIngredients.Add((recipe.Measures[i].Trim(), recipe.Ingredients[i].Trim()));
+                }
+            }
+
+            return formattedIngredients;
+        }
+
+        public async Task<(List<IngredientNutrition> IngredientsNutrition, NutritionInfo TotalNutrition)> CalculateRecipeNutrition(Recipe recipe)
+        {
+            var ingredientsNutrition = new List<IngredientNutrition>();
+            var totalNutrition = new NutritionInfo();
+
+            foreach (var (measure, ingredient) in GetFormattedIngredients(recipe))
+            {
+                var query = $"{measure} {ingredient}";
+                var nutrition = await _fatSecretClient.GetFoodNutritionAsync(query);
+
+                ingredientsNutrition.Add(new IngredientNutrition
+                {
+                    Ingredient = $"{measure} {ingredient}",
+                    Nutrition = nutrition
+                });
+
+                totalNutrition.Calories += nutrition.Calories;
+                totalNutrition.Protein += nutrition.Protein;
+                totalNutrition.Carbohydrates += nutrition.Carbohydrates;
+                totalNutrition.Fat += nutrition.Fat;
+            }
+
+            return (ingredientsNutrition, totalNutrition);
+        }
+    }
+
+    public class IngredientNutrition
+    {
+        public string Ingredient { get; set; }
+        public NutritionInfo Nutrition { get; set; }
+    }
+}
